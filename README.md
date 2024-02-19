89038639c8c59f0d7e42af283d1837449764c28b
Aline <alinefurette@gmail.com>
2024-02-19 16:27:52 +0100

    ecf

---
+++ README.md
@@ -0,0 +1,52 @@
+# ECF01-AlineROUSSEAU-WalletWizard
+
+This template should help get you started developing with Vue 3 in Vite.
+
+## Recommended IDE Setup
+
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+
+## Customize configuration
+
+See [Vite Configuration Reference](https://vitejs.dev/config/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
+
+ECF01 - Wallet Wizard
+Objectifs
+Créer une application web simple pour suivre les dépenses personnelles.
+Utiliser Vue.js pour construire l’interface utilisateur réactive.
+Maitriser la notion de composants.
+Sujet
+Vous devez développer une application de gestion des dépenses qui permettra aux utilisateurs de :
+Ajouter une nouvelle dépense avec une description et un montant.
+Afficher l'historique des dépenses enregistrées.
+Calculer le total des dépenses.
+Afficher les opérations de crédit et de débit indépendemment (argent entrant et sortant).
+Veillez à soigner l'UI/UX de l'application.
+Bonus (facultatif)
+Réaliser l'exercice en TypeScript.
+Stocker les données des dépenses dans le localStorage du navigateur.
+Ajoutez une date à chaque dépense et permettez aux utilisateurs de filtrer les dépenses par date.
+Rendu
+Le rendu se fera à l'aide un dépôt github avec le nommage suivant: ECF01-PrenomNOM-WalletWizard
+La rédaction d'un fichier readme.md contiendra les informations sur le projet et notamment la façon de
+lancer l'application
+Veillez à mettre vos dépôts en privé et à les rendre publics à la fin de l'épreuve: 17h30
+Tout commit réalisé après cet horaire ne sera pas pris en compte lors de l'évaluation
---
+++ src/App.vue
@@ -0,0 +1,89 @@
+<script setup>
+import { ref } from 'vue';
+const operations = ref({
+  type: 'crédit',
+  description: 'remboursement frais médicaux',
+  montant: '25',
+},
+{
+  type: 'débit',
+  description: 'achat chaussures',
+  montant: '75'
+  
+});
+;
+
+const application = {
+  data() {
+    return {
+      operations: window.operations,
+      newOperation: {
+
+      }
+    }
+  }
+}
+methods: {
+    addOperation: function()
+}
+</script>
+
+<template>
+  <header>
+<div id="logo">logo</div>
+  <h1>WALLET WIZARD</h1>  
+  </header>
+  <main>
+<div class="container main" id="app">
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">Type d'opération</th>
+        <th scope="col">Montant</th>
+        <th scope="col">Description</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(operation) in operations">
+        <td>{{ operation.type }}</td>
+        <td>{{ operation.description }}</td>
+        <td>{{ operation.montant }}</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+<form>
+  <div class="row">
+    <div class="col">
+    Crédit
+      <input type="checkbox" name="creditCheckbox" class="form-control" id="creditCheckbox">
+    </div>
+    <div class="col">
+      Débit
+      <input type="checkbox" name="debitCheckbox" class="form-control" id="debitCheckbox">
+    </div>
+      <br>
+    <div class="col">
+    Description :
+      <input type="text" name="description" class="form-control" id="descriptionOperation" v-model="newOperation.description">
+    </div>
+      <br>
+    <div class="col">
+    Montant :
+      <input type="text" name="montant" class="form-control" id="montantOperation" v-model="newOperation.montant">
+    </div>
+      <br>
+    <button type="submit" id="submit">AJOUTER</button>
+
+  </main>
+
+    <button type="submit" id="total">TOTAL</button>
+    <input type="text">
+  </div>
+</form>
+</template>
+
+<style scoped>
+
+</style>
